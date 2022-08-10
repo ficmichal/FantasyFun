cca7145dcc6bb5d8628149a557ff85bfd45bac46
ficmichal <meho.fic@gmail.com>
2022-08-11 00:04:25 +0200

    create db context

---
+++ src/FantasyFun.API/Repositories/FootballDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FantasyFun.API.Repositories
+{
+    public class FootballDbContext : DbContext
+    {
+        public const string DbPath = "data\\database.sqlite";
+        public readonly string ConnectionString;
+
+        public FootballDbContext()
+        {
+            var folder = Directory.GetCurrentDirectory();
+            ConnectionString = Path.Combine(folder, "..", "..", DbPath);
+        }
+
+
+    }
+}
