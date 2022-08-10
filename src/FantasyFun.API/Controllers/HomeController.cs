f2744fe76fe83d2341d71b7914f8bd80120d947c
Michal Fic <michal.fic@inexto.com>
2022-08-10 23:31:46 +0200

    add home controller

---
+++ src/FantasyFun.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FantasyFun.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetEntryMessage()
+        {
+            return "Welcome in FantasyFun!";
+        }
+    }
+}
--- src/FantasyFun.API/Program.cs
+++ src/FantasyFun.API/Program.cs
@@ -6,10 +6,13 @@ namespace FantasyFun.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
             app.UseRouting();
 
+            app.MapControllers();
             app.Run();
         }
     }
