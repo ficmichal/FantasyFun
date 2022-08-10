54dab240cee803573f0f18b2991930790f90f9cc
Michal Fic <michal.fic@inexto.com>
2022-08-10 23:26:39 +0200

    create plain API project

---
+++ src/FantasyFun.API/Program.cs
@@ -0,0 +1,16 @@
+namespace FantasyFun.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.Run();
+        }
+    }
+}
