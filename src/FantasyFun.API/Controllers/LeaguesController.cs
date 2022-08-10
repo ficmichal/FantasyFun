2db8806dda3bdbdae5048f1acdcce796ad941dad
ficmichal <meho.fic@gmail.com>
2022-08-11 00:29:56 +0200

    LeaguesController - get first league from db

---
+++ src/FantasyFun.API/Controllers/LeaguesController.cs
@@ -0,0 +1,20 @@
+﻿using FantasyFun.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasyFun.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaguesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAnyLeague()
+        {
+            var db = new FootballDbContext();
+
+            var anyLeague = await db.Leagues.FirstOrDefaultAsync();
+            return anyLeague.Name;
+        }
+    }
+}
