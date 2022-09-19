f4ea7a8ff2b6b2034332479d01a50f88886d056b
ficmichal <meho.fic@gmail.com>
2022-09-19 23:31:48 +0200

    expressions

--- src/FantasyFun.API/Controllers/PlayersController.cs
+++ src/FantasyFun.API/Controllers/PlayersController.cs
@@ -1,9 +1,12 @@
-﻿using FantasyFun.API.Repositories;
+﻿using FantasyFun.API.Models;
+using FantasyFun.API.Repositories;
 using FantasyFun.API.Settings;
 using FantasyFun.API.ViewModel;
+using FantasyFun.API.ViewModel.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace FantasyFun.API.Controllers
 {
@@ -39,7 +42,7 @@ namespace FantasyFun.API.Controllers
                 .ToListAsync();
 
             var anyPlayersWithOverall = anyPlayers
-                .Where(l => l.OverallRating == overall)
+                .Where(x => x.OverallRating == overall)
                 .Select(l => l.Name)
                 .Take(_InternalPlayersByOverallMaxNumber)
                 .ToList();
@@ -51,7 +54,63 @@ namespace FantasyFun.API.Controllers
 
             return Ok(anyPlayersWithOverall);
         }
-        
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<string>>> FilterPlayers(PlayerFilters playerFilters)
+        {
+            // Specification implements method IsSatisfied() whcih returns Expression<Func<PlayerAttribute>>
+            // FilterByNameSpecification : Specification
+            // var specBuilder = new PlayersSearchSpecification(PlayerFilters playerFilters);
+
+            // inside specBulider
+            // {
+            //  var specification = EmptySpec() : Specification;
+            //  if (players.Filters.Name is not null)
+            //  {
+            //     specification.And(new FilterByNameSpecification(playerFilters.Name)) 4
+            //  }else
+            //  {
+            //      // do nothing
+            //  }
+
+            // 
+
+
+
+            var anyPlayers = await _dbContext.PlayerAttributes
+                .Where(l => l.Date <= _defaultGameTime)
+                .Where(playerFilters.FilterByName())
+                .OrderByDescending(l => l.Date)
+                .GroupBy(l => l.Player.Name)
+                .Select(l => new PlayerType(l.FirstOrDefault().Player.Name, l.FirstOrDefault().OverallRating))
+                .Take(500)
+                .ToListAsync();
+
+            var anyPlayersWithOverall = anyPlayers
+                .Where(x => x.OverallRating == playerFilters.Overall)
+                .Select(l => l.Name)
+                .Take(_InternalPlayersByOverallMaxNumber)
+                .ToList();
+
+            if (anyPlayersWithOverall == null)
+            {
+                return NotFound(string.Empty);
+            }
+
+            return Ok(anyPlayersWithOverall);
+        }
+
+        //public Func<PlayerType, bool> FilterByOverall(long overall)
+        //{
+        //    return x => x.OverallRating == overall;
+        //}
+
+        //public Expression<Func<Player_Attribute, bool>> FilterByGameTime()
+        //{
+        //    return l => l.Date <= _defaultGameTime;
+        //}
+
         [HttpGet]
         [Route("{id}")]
         public async Task<ActionResult<PlayerType>> GetPlayersByOverall(int id)
---
+++ src/FantasyFun.API/ViewModel/Filters/PlayerFilters.cs
@@ -0,0 +1,19 @@
+﻿using FantasyFun.API.Models;
+using System.Linq.Expressions;
+
+namespace FantasyFun.API.ViewModel.Filters
+{
+    public class PlayerFilters
+    {
+        public long Overall { get; set; }
+
+        public string Name { get; set; }
+
+        public Expression<Func<Player_Attribute, bool>> FilterByName()
+        {
+            return pa => string.IsNullOrEmpty(Name) 
+                ? true 
+                : pa.Player.Name.Contains(Name);
+        }
+    }
+}
