80c91a140538364ed5398223b3242f0beb68f1d6
ficmichal <meho.fic@gmail.com>
2022-09-20 20:34:46 +0200

    create dummy interfaces returning our names, Make it internal - hide implementation!

---
+++ src/FantasyFun.Application/DummyPlayersServices.cs
@@ -0,0 +1,23 @@
+﻿namespace FantasyFun.Application
+{
+    internal class WojewodaPlayersService : IPlayersService
+    {
+        public Task<IEnumerable<string>> GetPlayersByOverall(long overall)
+        {
+            return Task.FromResult(new List<string> {"Mikołaj Wojewoda"}.AsEnumerable());
+        }
+
+        public bool CheckLewandowski(string value)
+        {
+            return value.ContainsLewandowski();
+        }
+    }
+
+    internal class FicPlayersService : IPlayersService
+    {
+        public Task<IEnumerable<string>> GetPlayersByOverall(long overall)
+        {
+            return Task.FromResult(new List<string> {"Michał Fic"}.AsEnumerable());
+        }
+    }
+}
