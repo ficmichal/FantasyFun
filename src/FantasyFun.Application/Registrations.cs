56223316d0bee5853df232ec2fe9f840b3f3c21f
ficmichal <meho.fic@gmail.com>
2022-09-20 20:35:35 +0200

    register dummy services

---
+++ src/FantasyFun.Application/Registrations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FantasyFun.Application
+{
+    public static class Registrations
+    {
+        public static void RegisterDummyNamedPlayerService(this IServiceCollection services, string name)
+        {
+            if (name is "Mikołaj Wojewoda")
+            {
+                services.AddScoped<IPlayersService, WojewodaPlayersService>();
+            }
+            else
+            {
+                services.AddScoped<IPlayersService, FicPlayersService>();
+            }
+        }
+    }
+}
