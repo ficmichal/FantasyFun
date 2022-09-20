02ea6f8e1a75b159f3de49348189aa5b2f201dcd
ficmichal <meho.fic@gmail.com>
2022-09-20 20:35:23 +0200

    learn Extension methods

---
+++ src/FantasyFun.Application/SampleExtensionsBasedOnString.cs
@@ -0,0 +1,10 @@
+﻿namespace FantasyFun.Application
+{
+    public static class SampleExtensionsBasedOnString
+    {
+        public static bool ContainsLewandowski(this string value)
+        {
+            return value.Contains("Lewandowski");
+        }
+    }
+}
